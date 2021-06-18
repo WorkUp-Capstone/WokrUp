ced2b946941091797ddecfdf75a4b0af97a4d2b4
taylor lindholm <taylor.lindholm01@gmail.com>
2021-06-18 13:18:58 -0500

    feat: ide setup

---
+++ src/main/java/com/workup/workup/WorkUpApplication.java
@@ -0,0 +1,13 @@
+package com.workup.workup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workup/workup/WorkUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workup.workup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
