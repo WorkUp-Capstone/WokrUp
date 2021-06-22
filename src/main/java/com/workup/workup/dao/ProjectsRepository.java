22ba768ceedf665de961105c46de3c609cb01267
Alex <thoms.alexander23@gmail.com>
2021-06-22 16:30:29 -0400

    Created and filled in with project information the category, image, status and project classes; Not that date aspects of the project are still up in the air and categories should be linked to users as well

--- src/main/java/com/workup/workup/dao/ProjectsRepository.java
+++ src/main/java/com/workup/workup/dao/ProjectsRepository.java
@@ -1,7 +1,10 @@
-package dao;
+package com.workup.workup.dao;
 
+
+import com.workup.workup.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 // PROJECT REPOSITORY
