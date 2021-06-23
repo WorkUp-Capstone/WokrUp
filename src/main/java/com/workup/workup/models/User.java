da0028d0c574531f7d678de8aafe8f43b5d52063
Alex Avitia <alex.avitia77@gmail.com>
2021-06-23 09:26:14 -0500

    added constructor and getters/setters for user.java

--- src/main/java/com/workup/workup/models/User.java
+++ src/main/java/com/workup/workup/models/User.java
@@ -11,6 +11,62 @@ public class User {
     }
 
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(String role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
     //Insert Constructor
     public User(long id, String role_id, String email, String password, String first_name, String last_name)
     {
@@ -22,7 +78,14 @@ public class User {
         this.last_name = last_name;
     }
 
-
+    public User(String role_id, String email, String password, String first_name, String last_name)
+    {
+        this.role_id = role_id;
+        this.email = email;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
