2cca1f15bcd037dbe7f6858d4bdff570cdc34fbf
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-25 15:07:04 -0500

    reverted getMapping for welcome

--- src/main/java/com/workup/workup/controllers/HomeController.java
+++ src/main/java/com/workup/workup/controllers/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String welcome(){
         return "welcome";
     }
