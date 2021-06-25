86598b1f9e8ca5d333b209142996a4382a094109
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-06-24 23:56:23 -0500

    Feat: Adding finding profile by title

--- src/main/java/com/workup/workup/dao/ProfileRepository.java
+++ src/main/java/com/workup/workup/dao/ProfileRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Profile findById(long id);
+
+
+    Profile findProfileByTitle(String title);
 }
