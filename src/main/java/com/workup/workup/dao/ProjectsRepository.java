55e3cd82e46b446268fe4b53a97d1caa0eb75d18
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-06-24 23:52:01 -0500

    Feat: basic findBy.. Also importing util.List

--- src/main/java/com/workup/workup/dao/ProjectsRepository.java
+++ src/main/java/com/workup/workup/dao/ProjectsRepository.java
@@ -5,10 +5,19 @@ import com.workup.workup.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 // PROJECT REPOSITORY
 // PROJECT MODEL NOT CREATED OR CONNECTED YET! DELETE WHEN CONNECTED!!!! //
 public interface ProjectsRepository extends JpaRepository<Project, Long> {
+
+    Project findByTitle(String title); // select * from ads where title = ?
+    Project findFirstByTitle(String title); // select * from ads where title = ? limit 1
+    Project findByTitleLike(String searchPattern);  //find by title "%INPUT%"
+
+    List<Project> searchByTitle(String term);
+
 
 }
