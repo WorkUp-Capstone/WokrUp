3073bf8210493a7e775ec8f0c1f0e052eedba684
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-25 18:50:56 -0500

    DO NOT MERGE-Still a work in progress and need to confirm fields and table relationships to display correct information

--- src/main/java/com/workup/workup/controllers/HomeController.java
+++ src/main/java/com/workup/workup/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.workup.workup.controllers;
 
+import com.workup.workup.dao.ProfileRepository;
+import com.workup.workup.dao.ProjectsRepository;
+import com.workup.workup.dao.UsersRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+    private ProjectsRepository projectsDao;
+    private ProfileRepository profileDao;
+    private UsersRepository usersDao;
+
+    public HomeController(ProjectsRepository projectsRepository, ProfileRepository profileRepository, UsersRepository usersRepository){
+        projectsDao = projectsRepository;
+        profileDao = profileRepository;
+        usersDao = usersRepository;
+    }
+
     @GetMapping("/")
     public String welcome(){
         return "welcome";
@@ -19,6 +32,23 @@ public class HomeController {
     }
 
     // TODO: post mapping for register
+
+
+    //Project index for Developers to view in their Home Page
+    @GetMapping("/home-projects")
+    public String projectsIndex(Model model){
+        model.addAttribute("allProjects", projectsDao.findAll());
+        return "projects/index";
+    }
+
+    //Project index for Developers to view in their Home Page
+    @GetMapping("/home-developers")
+    public String developersIndex(Model model){
+        model.addAttribute("allDevs", usersDao.findByRole()); //need to find relationship for developer roles to pass in the parameter (this is most likely wrong)
+        return "users/owner-profile";
+    }
+
+
 
 
 }
