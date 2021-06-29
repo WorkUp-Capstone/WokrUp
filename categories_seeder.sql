20330cbe3d8d28739f893fe4cf94f828ef5b5e61
Alex <thoms.alexander23@gmail.com>
2021-06-29 11:45:26 -0400

    Add: seeder for initital categories generation

---
+++ categories_seeder.sql
@@ -0,0 +1,5 @@
+
+USE workup_db;
+
+INSERT INTO categories(name)
+VALUES ('Front-end development'), ('Back-end development'), ('Full-stack development'), ('MySQL'), ('CSS'), ('HTML'), ('Spring'), ('Java'), ('JQuery'), ('JavaScript'), ('AJAX'), ('Authentication'), ('E-mail service'), ('Database'), ('UI/UX'), ('RESTful API');
