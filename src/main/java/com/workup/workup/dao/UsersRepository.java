c53c43d16e69b3182e0420b70910fc3722b63a5b
Alex Avitia <alex.avitia77@gmail.com>
2021-06-29 16:08:32 -0500

    added main framework files for messenger system, needs to access user storage

--- src/main/java/com/workup/workup/dao/UsersRepository.java
+++ src/main/java/com/workup/workup/dao/UsersRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 // USER REPOSITORY
 // USER MODEL NOT CONNECTED YET! DELETE WHEN CONNECTED!!!! //
 public interface UsersRepository extends JpaRepository<User, Long> {
+
     User findByUsername(String email);
 
 }
