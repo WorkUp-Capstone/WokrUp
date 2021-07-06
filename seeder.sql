c50e6cb79b8aa8eefdf49e5e6ac5dfc37ffb0cdc
Alex Avitia <alex.avitia77@gmail.com>
2021-07-05 20:48:24 -0500

    removed duplicate getRole()

--- seeder.sql
+++ seeder.sql
@@ -1,7 +1,12 @@
-
 USE workup_db;
+
+INSERT INTO role(role)
+VALUES('owner'), ('developer');
+
+INSERT INTO status(status)
+VALUES('open'), ('in_progress');
 
 INSERT INTO categories(name)
 VALUES ('Front_end_development'), ('Back_end_development'), ('Full_stack_development'), ('MySQL'), ('CSS'), ('HTML'), ('Spring'), ('Java'), ('JQuery'), ('JavaScript'), ('AJAX'), ('Authentication'), ('Email_service'), ('Database'), ('UI_UX'), ('RESTful_API');
 
-/* migration.sql now has all necessary code to populate role, status, and categories */
+/* Run migration.sql first */
