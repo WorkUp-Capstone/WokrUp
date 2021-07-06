d3ac4ac0872c28acdf6a6b80bc118e0719bacdeb
Alex Avitia <alex.avitia77@gmail.com>
2021-07-05 20:20:12 -0500

    feat: refactored 'workUp_db_creation.sql' to 'migration.sql' and included seeders for 'role', 'status', and 'categories' tables. No longer need 'categories_seeder.sql'

--- categories_seeder.sql
+++ categories_seeder.sql
@@ -3,3 +3,5 @@ USE workup_db;
 
 INSERT INTO categories(name)
 VALUES ('Front_end_development'), ('Back_end_development'), ('Full_stack_development'), ('MySQL'), ('CSS'), ('HTML'), ('Spring'), ('Java'), ('JQuery'), ('JavaScript'), ('AJAX'), ('Authentication'), ('Email_service'), ('Database'), ('UI_UX'), ('RESTful_API');
+
+/* migration.sql now has all necessary code to populate role, status, and categories */
