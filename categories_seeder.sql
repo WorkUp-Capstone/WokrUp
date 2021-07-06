49fc3e4e21804020349a5daedbf18bba6108309d
Alex <thoms.alexander23@gmail.com>
2021-07-06 00:45:54 -0400

    Feat: categories now display properly for projects on creation, profile, and home views

--- categories_seeder.sql
+++ categories_seeder.sql
@@ -1,5 +1,10 @@
 
 USE workup_db;
+INSERT INTO role(role)
+VALUES ('project_owner'), ('developer');
+
+INSERT INTO status(status)
+VALUES ('open'), ('in progress');
 
 INSERT INTO categories(name)
 VALUES ('Front_end_development'), ('Back_end_development'), ('Full_stack_development'), ('MySQL'), ('CSS'), ('HTML'), ('Spring'), ('Java'), ('JQuery'), ('JavaScript'), ('AJAX'), ('Authentication'), ('Email_service'), ('Database'), ('UI_UX'), ('RESTful_API');
