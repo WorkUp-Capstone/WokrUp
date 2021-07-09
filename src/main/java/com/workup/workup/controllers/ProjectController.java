fe330e7281e81356c88c83cc0e05ce1dcd2f52ff
Alex <thoms.alexander23@gmail.com>
2021-07-09 13:32:58 -0400

    Feat: completed basic implementation of search for developer view and started owner view

--- src/main/java/com/workup/workup/controllers/ProjectController.java
+++ src/main/java/com/workup/workup/controllers/ProjectController.java
@@ -1,9 +1,11 @@
 package com.workup.workup.controllers;
 
 import com.workup.workup.dao.CategoryRepository;
+import com.workup.workup.dao.ProfileRepository;
 import com.workup.workup.dao.ProjectsRepository;
 import com.workup.workup.dao.UsersRepository;
 import com.workup.workup.models.Category;
+import com.workup.workup.models.Profile;
 import com.workup.workup.models.Project;
 import com.workup.workup.models.User;
 import com.workup.workup.services.EmailService;
@@ -12,8 +14,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-//import com.workup.workup.models.Status;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 import java.sql.Date;
 import java.util.List;
 
@@ -22,26 +24,29 @@ import java.util.List;
 public class ProjectController {
 
     private final CategoryRepository categoryDao;
-private final ProjectsRepository projectDao;
-private final EmailService emailService;
-private final UsersRepository userDao;
+    private final ProjectsRepository projectDao;
+    private final EmailService emailService;
+    private final UsersRepository userDao;
+    private final ProfileRepository profilesDao;
 
-public ProjectController(CategoryRepository categoryDao, ProjectsRepository projectDao, EmailService emailService, UsersRepository userDao){
-    this.categoryDao = categoryDao;
-    this.projectDao = projectDao;
-    this.emailService = emailService;
-    this.userDao = userDao;
-
-}
+    public ProjectController(CategoryRepository categoryDao, ProjectsRepository projectDao, EmailService emailService, UsersRepository userDao, ProfileRepository profilesDao) {
+        this.categoryDao = categoryDao;
+        this.projectDao = projectDao;
+        this.emailService = emailService;
+        this.userDao = userDao;
+        this.profilesDao = profilesDao;
+    }
 
     //display ALL projects
     @GetMapping("/owner-profile/projects")
-    public String projectsIndex(Model model){
+    public String projectsIndex(Model model) {
         model.addAttribute("allProjects", projectDao.findAll());
         return "projects/index"; // ?? may need return refactor
     }
 
-/**Lines below commented in the event we need to show one project outside of the card view in the projects' index (dev/home) *note that show.html does not exist*/
+    /**
+     * Lines below commented in the event we need to show one project outside of the card view in the projects' index (dev/home) *note that show.html does not exist
+     */
     //display selected single project
 //    @GetMapping("/owner-profile/projects/{id}")
 //    public String showProject(@PathVariable long id, Model model){
@@ -51,7 +56,7 @@ public ProjectController(CategoryRepository categoryDao, ProjectsRepository proj
 
     //create a Project
     @GetMapping("/owner-profile/projects/create")
-    public String viewProjectCreateForm(Model model, Model categoryModel){
+    public String viewProjectCreateForm(Model model, Model categoryModel) {
         model.addAttribute("project", new Project());
         categoryModel.addAttribute("categoryList", categoryDao.findAll());
         return "projects/create";
@@ -59,11 +64,11 @@ public ProjectController(CategoryRepository categoryDao, ProjectsRepository proj
 
     @PostMapping("/projects/create")
     public String createProject(
-                              @RequestParam(name = "title") String title,
-                              @RequestParam(name = "description") String description,
-                              @RequestParam(name = "categories") List<Category> categoryList,
-                              @RequestParam(name = "status") String status,
-                              @AuthenticationPrincipal User user) {
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "description") String description,
+            @RequestParam(name = "categories") List<Category> categoryList,
+            @RequestParam(name = "status") String status,
+            @AuthenticationPrincipal User user) {
 
         Project newProject = new Project();
         newProject.setTitle(title);
@@ -82,7 +87,7 @@ public ProjectController(CategoryRepository categoryDao, ProjectsRepository proj
 
     //edit selected project
     @GetMapping("/projects/{id}/edit")
-    public String editProjectForm(Model model, @PathVariable Long id){
+    public String editProjectForm(Model model, @PathVariable Long id) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Project projectToEdit = projectDao.getById(id);
         model.addAttribute("editProject", projectToEdit);
@@ -90,15 +95,18 @@ public ProjectController(CategoryRepository categoryDao, ProjectsRepository proj
     }
 
     //edit and save project
-    /** TODO: need to include @RequestParams for categories and possibly files? */
+
+    /**
+     * TODO: need to include @RequestParams for categories and possibly files?
+     */
 
     @PostMapping("/projects/{id}/edit")
     public String editProject(@PathVariable Long id,
-                              @RequestParam(name="title") String title,
-                              @RequestParam(name="description") String description,
-                              @RequestParam(name="status") String status
+                              @RequestParam(name = "title") String title,
+                              @RequestParam(name = "description") String description,
+                              @RequestParam(name = "status") String status
                               //,@RequestParam(name="categories")Category categories
-                              ){
+    ) {
 
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
@@ -112,29 +120,50 @@ public ProjectController(CategoryRepository categoryDao, ProjectsRepository proj
 
         projectDao.save(project);
         return "redirect:/owner-profile";
-
     }
 
 
-    public List<Project> productSearch(String searchString){
-        return projectDao.productSearch(searchString);
+//      METHODS NEEDED FOR SEARCH TO WORK DECENT
+//    IMPROVEMENTS THAT ARE NEEDED ARE PARTIAL/FUZZY INTERPRETATION AND STATE IS ACTING FUNNY
+    public List<Long> projectSearch(String searchString) {
+        return projectDao.projectSearch(searchString);
+    }
+
+    public List<Long> devSearch(String searchString) {
+        return profilesDao.devSearch(searchString);
     }
 
     @GetMapping("/search")
-    public String search(@Param("searchString") String keyword, Model model){
-        model.addAttribute("searchString", keyword);
-        model.addAttribute("pageTitle", "Search Results for '" + keyword + "'");
-
-        List<Project> searchResults = productSearch(keyword);
-        model.addAttribute("searchResults", searchResults);
-
+    public String search(@Param("searchString") String keyword, Model model, @AuthenticationPrincipal User user) {
+        if (user.getRole().getRole().equalsIgnoreCase("developer")) {
+            List<Long> searchResult = projectSearch(keyword);
+            if (searchResult.isEmpty()) {
+                List<Project> searchResults = projectDao.findAll();
+                model.addAttribute("searchResults", searchResults);
+                model.addAttribute("searchString", "No Results for '" + keyword + "'");
+                model.addAttribute("pageTitle", "No Results for '" + keyword + "'");
+            } else {
+                List<Project> searchResults = projectDao.findAllById(searchResult);
+                model.addAttribute("searchResults", searchResults);
+                model.addAttribute("searchString", "Search Results for '" + keyword + "'");
+                model.addAttribute("pageTitle", "Search Results for '" + keyword + "'");
+            }
+        } else {
+            List<Long> searchResult = devSearch(keyword);
+            if (searchResult.isEmpty()) {
+                List<Profile> searchResults = profilesDao.findAll();
+                model.addAttribute("searchResults", searchResults);
+                model.addAttribute("searchString", "No Results for '" + keyword + "'");
+                model.addAttribute("pageTitle", "No Results for '" + keyword + "'");
+            } else {
+                List<Profile> searchResults = profilesDao.findAllById(searchResult);
+                model.addAttribute("searchResults", searchResults);
+                model.addAttribute("searchString", "Search Results for '" + keyword + "'");
+                model.addAttribute("pageTitle", "Search Results for '" + keyword + "'");
+            }
+        }
         return "projects/search_result";
     }
-
-//    need to discuss postmapping redirect plan as well as confirm how we will handle search ie. categories/description
-//   public String searchProject(@PathVariable String searchString, Model model){
-//
-//    }
 
 
 }
--- src/main/java/com/workup/workup/dao/ProfileRepository.java
+++ src/main/java/com/workup/workup/dao/ProfileRepository.java
@@ -4,9 +4,12 @@ import com.workup.workup.models.Profile;
 import com.workup.workup.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Profile findById(long id);
 
     Profile getProfileByUserIs(User user);
 
+    List<Long> devSearch(String searchString);
 }
