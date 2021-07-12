af70edd08161e6e1c09f2c44abc09dcba04e2416
Alex Avitia <alex.avitia77@gmail.com>
2021-07-12 11:39:23 -0500

    Feat: added a keys controller and class config

--- seeder.sql
+++ seeder.sql
@@ -6,3 +6,11 @@ INSERT INTO categories(name)
 VALUES ('Front End Development'), ('Back End Development'), ('Full Stack Development'), ('MySQL'), ('CSS'), ('HTML'), ('Spring'), ('Java'), ('JQuery'), ('JavaScript'), ('AJAX'), ('Authentication'), ('Email_service'), ('Database'), ('UI_UX'), ('RESTful API');
 
 /* Run migration.sql first */
+
+UPDATE categories
+SET
+    'name'
+WHERE
+    'Front_end_development' = 'Front End Development',
+    'Back_end_development' = 'Back End Development',
+    'Full_stack_development' = 'Full Stack Development';
--- src/main/java/com/workup/workup/WorkUpApplication.java
+++ src/main/java/com/workup/workup/WorkUpApplication.java
@@ -1,8 +1,13 @@
 package com.workup.workup;
 
+import com.workup.workup.models.ConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class WorkUpApplication {
 
