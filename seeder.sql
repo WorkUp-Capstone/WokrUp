36ebad7b918764c19e94534fb0cd1adb28f1c96a
Alex Avitia <alex.avitia77@gmail.com>
2021-07-12 15:48:51 -0500

    edited seeder

--- seeder.sql
+++ seeder.sql
@@ -3,6 +3,6 @@ INSERT INTO role(role)
 VALUES ('owner'), ('developer');
 
 INSERT INTO categories(name)
-VALUES ('Front End Development'), ('Back End Development'), ('Full Stack Development'), ('MySQL'), ('CSS'), ('HTML'), ('Spring'), ('Java'), ('JQuery'), ('JavaScript'), ('AJAX'), ('Authentication'), ('Email_service'), ('Database'), ('UI_UX'), ('RESTful API');
+VALUES ('Front End Development'), ('Back End Development'), ('Full Stack Development'), ('MySQL'), ('CSS'), ('HTML'), ('Spring'), ('Java'), ('JQuery'), ('JavaScript'), ('AJAX'), ('Authentication'), ('Email service'), ('Database'), ('UI/UX'), ('RESTful API');
 
 /* Run migration.sql first */
