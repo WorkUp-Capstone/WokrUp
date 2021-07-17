4cbf38892e12922a604be0bd96f58fde836717cb
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-07-16 22:52:25 -0500

    login redirect to profile, antMatchers added

--- src/main/java/com/workup/workup/configuration/SecurityConfiguration.java
+++ src/main/java/com/workup/workup/configuration/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /** Login configuration */
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/") // user's home page, it can be any URL
+                .defaultSuccessUrl("/profile") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
 
                 /** Logout configuration */
@@ -61,7 +61,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/profile",
                         "/profile/view-prospect",
                         "/profile/edit",
-                        "/home"
+                        "/home",
+                        "/profile/profileImg/add",
+                        "profile/projects/{id}/add",
+                        "home/view-prospect"
+
                 )
                 .authenticated()
         ;
