601460112fea78bae9fab87c93dab1574a95ffcb
Alex <thoms.alexander23@gmail.com>
2021-08-11 21:30:05 -0400

    fix: checkstyle warnings addressed and fixed as neccesary, annotations ignore the Javadoc comment rules

--- src/main/java/com/workup/workup/controllers/KeysController.java
+++ src/main/java/com/workup/workup/controllers/KeysController.java
@@ -4,9 +4,9 @@ import com.workup.workup.models.ConfigProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Map;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
 
 @Controller
 public class KeysController {
--- src/main/java/com/workup/workup/dao/ImagesRepository.java
+++ src/main/java/com/workup/workup/dao/ImagesRepository.java
@@ -1,11 +1,8 @@
 package com.workup.workup.dao;
 
-import com.workup.workup.models.Project;
 import com.workup.workup.models.ProjectImage;
-import com.workup.workup.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImagesRepository extends JpaRepository<ProjectImage, Long> {
 
